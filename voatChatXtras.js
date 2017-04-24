d7f56c025b41b25fa85cb3ffa4efdebe8a35ef36
EngelberryHumperdingle <cactusowlmoongoat@manbearpig.com>
2017-04-24 02:14:54 -0400

    clean

--- voatChatXtras.js
+++ voatChatXtras.js
@@ -1,11 +1,7 @@
 // voatChatXtras.js
 // https://voat.co/user/EngelbertHumperdinck
-//
-// version 2.14.11
-// updated updateLabels function
 
-// next:
-// removed console calls, added comments
+/////////////////////////////////////////////////
 
 var blockedUserList = [];
 var fakeList = { labelName: 'fake', labelColor: 'rgb(255, 0, 0)', list: [] };
@@ -88,33 +84,6 @@ function updateLabels(){
 
 }
 
-function initiateUserLabels(){
-
-  $(labelLists).each(function(){
-
-    // write a new css rule for each user on the list
-    var theListHTML = [];
-
-    if ( this.list.length > 0 ) {
-      $(this.list).each(function(){
-        theListHTML.push('div.chat-message-head a[href="/user/'+this+'"]' );
-
-      })  
-      $('style#'+this.labelName).html(theListHTML + '{color: '+this.labelColor+'}');
-    }
-    else { 
-      // console.log('no list for '+this.labelName); 
-      $('style#'+this.labelName).html("");
-    }
-
-  });
-
-  $(labelLists).each(function(){
-    // console.log('\t'+this.labelName+': '+this.list);
-  });
-}
-
-
 /////////////////////////////////////////////////
 
 // click block buttons
@@ -154,7 +123,6 @@ $('body').on('click', '.labelOptions a', function(){
   var userName = theUser.split('/').pop();
   var theList = {};
   var theOtherLists = [];
-  // var theStyle = "";
   var theLabel = $(this).text();
   var userColor = $(this).parents('p').find('a').css('color');
   var labelColor = "";
@@ -163,69 +131,48 @@ $('body').on('click', '.labelOptions a', function(){
     case 'fake' : 
       theList = fakeList;
       theOtherLists = [shillList, broList];
-      // theStyle = "{color: rgb(255, 0, 0)};";
       labelColor = "rgb(255, 0, 0)";
       break;
     case 'shill' : 
       theList = shillList;
       theOtherLists = [fakeList, broList];
-      // theStyle = "{color: rgb(7, 82, 165);};";
       labelColor = "rgb(7, 82, 165)";
       break;
     case 'bro' : 
       theList = broList;
       theOtherLists = [fakeList, shillList];
-      // theStyle = "{color: rgb(6, 115, 57)};";
       labelColor = "rgb(6, 115, 57)";
       break;
     default :
-      // no list
-      theOtherLists = [fakeList, shillList, broList];
-      // theStyle = "{color: rgb(86, 168, 218)};";
+      // this will never happen
+      
+      // default voat color
       labelColor = "rgb(86, 168, 218)";
   }
-
-  // console.log('\n\ntheList: '+theList.labelName+'\nuserColor: '+userColor+', labelColor: '+labelColor);
 
   // if the user is already on the selected list
   if (userColor == labelColor) {
     // remove user from selected list
     theList.list.splice(theList.list.indexOf(userName), 1);
 
-    // console
-    // console.log('\n removed '+userName+' from list: '+ theLabel);
-    //if (theList.list.length) console.log('\t theList.length: '+theList.list.length);
-    //else console.log('\t empty list');
-    // console.log('\t'+theList.list);
-
     // if the selected list is empty, remove it from localStorage
     if (theList.list.length < 1) localStorage.clear(theLabel);
   }
   else {
-    // console.log('user: '+ userName+' not on list: '+theList.labelName);
     // remove user from the other lists if present
     $(theOtherLists).each(function(i){
-      // console.log('checking: '+this.labelName+' for: '+userName);
-      // console.log(this.list.indexOf(userName) != -1);
-
       // if the user is on the list
       if ( this.list.indexOf(userName) != -1 ) {
-        // console.log(userName+' is on list: '+theOtherLists[i].labelName);
         this.list.splice(this.list.indexOf(userName), 1);
-        // console.log(userName + ' removed from list: '+this.labelName);
       }
-
     });
 
     // add it to the new list
     theList.list.push(userName);
-    // console.log('\n added '+userName+' to list: '+ theLabel);
-    // console.log('\t'+ theLabel+ '.length: '+theList.list.length);
-    // console.log('\t'+theList.list);
   }
 
-  //updateUserLabels(userName, theList, theOtherLists, theLabel);
   updateLabels();
+
 });
 
 /////////////////////////////////////////////////
@@ -247,58 +194,37 @@ $('document').ready(function(){
   // add area to display blocked users
   $('.sidecontentbox').append(blockListDisplay);
   
-  // get lists from localStorage
-  // try { console.log('blocked: '+localStorage.getItem('blocked')); }
-  // catch(e) {}
-
   if (localStorage.getItem('blocked') != null) {
     blockedUserList = localStorage.getItem('blocked').split(',');
     updateBlockList();
   }
-  //else { console.log('no blocked users'); }
-
-  // try { console.log('fake: '+localStorage.getItem('fake')); }
-  // catch(e) {}
-
+  
   if (localStorage.getItem('fake') != null) {
     fakeList.list = localStorage.getItem('fake').split(',');
-    // console.log('fakeList: '+fakeList.list);
   }
-  //else { console.log('no fakes'); }
-
-  // try { console.log('shill: '+localStorage.getItem('shill')); }
-  // catch(e) {}
-
+  
   if (localStorage.getItem('shill') != null) {
     shillList.list = localStorage.getItem('shill').split(',');
-    // console.log('shillList: '+shillList.list);
   }
-  //else { console.log('no shills'); }
-
-  // try { console.log('bro: '+localStorage.getItem('bro')); }
-  // catch(e) {}
-
+  
   if (localStorage.getItem('bro') != null) {
     broList.list = localStorage.getItem('bro').split(',');
-    // console.log('broList: '+broList.list);
   }
-  //else { console.log('no bros'); }
-
+  
   if ( (localStorage.getItem('fake') != null) ||
       (localStorage.getItem('shill') != null) ||
       (localStorage.getItem('bro') != null) ) {
-    // initiateUserLabels();
     updateLabels();
   }
-  //else { console.log('no labels in localStorage'); }
-   
+
+
+  //////////////////////////////////////////////////////// 
   // update display each time a new comment is added
   $('.chatContent').bind("DOMSubtreeModified",function(){
 
      // add buttons to newly added comment
      if ($('.chat-message').length > numComments) {
        numComments = $('.chat-message').length;
-       //$('.chat-message:last').find('.chat-message-head p').append(blockButton).append(fakeButton).append(coolButton);
        $('.chat-message:last').find('.chat-message-head p').append(blockButton).append(labelButton);
     
        updateBlockList();
