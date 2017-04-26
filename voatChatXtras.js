537ae6324825c14d88a0fd776827fb5251cef5ef
EngelberryHumperdingle <cactusowlmoongoat@manbearpig.com>
2017-04-26 10:10:58 -0400

    custom lists 0.15.15

--- voatChatXtras.js
+++ voatChatXtras.js
@@ -1,7 +1,7 @@
 // voatChatXtras.js
 // https://voat.co/user/EngelbertHumperdinck
 
-console.log('custom lists 0.15.14 ');
+console.log('custom lists 0.15.15 ');
 
 // to do:
 // √ don't allow users on more than one list
@@ -185,7 +185,7 @@ function updateLabelsX(){
           }
 
           // add the styles to the page
-          $('style#'+key).html(theListCSS.join(',') + '{color: '+labelListsX[key].labelColor+'; background-color: rgba(255,255,255,0.3); margin-right: 20px; '+ (specialIcon == "" ? '}' : 'margin-right: 18px; position: relative; }') );
+          $('style#'+key).html(theListCSS.join(',') + '{color: '+labelListsX[key].labelColor+'; background-color: rgba(255,255,255,0.3);'+ (specialIcon == "" ? ' }' : ' margin-right: 18px; position: relative; }') );
 
           if (specialIcon != "") {
             $('style#'+key).append(specialCSS.join(',') + '{content: "' + specialIcon + '"; color: '+specialIconColor+'; font-size: 1.5em; line-height: 0.8em; position: absolute; margin-left: 5px; top: 0px; }');
